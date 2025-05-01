0776661a657d2f3b65e3c02cc9c2c84410b48c4c
mmorsy16 <mahmoud.morsy200416@gmail.com>
2025-05-01 17:14:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,24 @@
 from flask import Flask, render_template, request
+import boto3
 from werkzeug.utils import secure_filename
-import boto3, os
 
-app = Flask(__name__,
-            template_folder='templates',
-            static_folder='static')
-
-# ←– your bucket & region
-S3_BUCKET = 'file-share-s3-storage-may5-2025'
-S3_REGION = 'eu-west-2'
-
-s3 = boto3.client('s3', region_name=S3_REGION)
-ALLOWED_EXT = {'.txt','.pdf','.png','.jpg','.jpeg','.gif'}
-
-def allowed_file(filename):
-    return os.path.splitext(filename)[1].lower() in ALLOWED_EXT
+app = Flask(__name__)
+s3 = boto3.client('s3')
+BUCKET = 'my-file-sharing-bucket-2025'
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    f = request.files.get('file')
-    if not f or not allowed_file(f.filename):
-        return "Invalid file type", 400
-
-    filename = secure_filename(f.filename)
-    s3.upload_fileobj(f, S3_BUCKET, filename)
-
-    url = s3.generate_presigned_url(
-        'get_object',
-        Params={'Bucket': S3_BUCKET, 'Key': filename},
-        ExpiresIn=3600
-    )
-    return render_template('upload_success.html',
-                           filename=filename, url=url)
+    file = request.files['file']
+    if file:
+        filename = secure_filename(file.filename)
+        s3.upload_fileobj(file, BUCKET, filename)
+        url = s3.generate_presigned_url('get_object', Params={'Bucket': BUCKET, 'Key': filename}, ExpiresIn=3600)
+        return f'Download Link: <a href="{url}">{url}</a>'
+    return 'Upload failed!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
-            app.run(host='0.0.0.0', port=5000)
-
-
